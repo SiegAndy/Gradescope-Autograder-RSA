81b73cb616547b82c816978b8ee6aaba7e43fb68
Annie <zc470618625@gmail.com>
2023-10-06 13:14:14 -0400

    Allow partial credit

--- src/tests/test_outputs.py
+++ src/tests/test_outputs.py
@@ -9,6 +9,7 @@ from gradescope_utils.autograder_utils.decorators import (
     tags,
     visibility,
     weight,
+    partial_credit,
 )
 
 import rsa as RSA
@@ -38,7 +39,6 @@ class TestOutput(unittest.TestCase):
     allowed_imports: List[str]
 
     def setUp(self):
-        self.rsa = RSA.RSA()
         self.allowed_imports = ["RSA"]
         with open("grading.json", "r") as grading_file:
             self.test_params = json.load(
@@ -62,73 +62,101 @@ class TestOutput(unittest.TestCase):
                 f"Import not allowed: <{name}>",
             )
 
-    @weight(30)
+    @partial_credit(30)
     @tags("output")
     @visibility("after_due_date")
     @number("2.1")
-    def test_key_generation(self):
+    def test_key_generation(self, set_score=None):
         """
         Compute Key using given p and q, and check class attributes have been correctly set.
         """
         self.import_checker()
-
+        self.rsa = RSA.RSA()
+        no_param = set()
+        wrong_param = set()
+        error_param = set()
         for param in self.test_params.compute_key:
             self.rsa.compute_key(param.p, param.q, param.cp_index)
+            # each class attributes is 5 credit
             for pkey in ["p", "q", "n", "phi", "e", "d"]:
-                self.assertTrue(
-                    hasattr(self.rsa, pkey), f"Checking class has the attribute {pkey}"
-                )
-                self.assertEqual(
-                    self.rsa.__dict__[pkey],
-                    param.__dict__[pkey],
-                    f"Checking class attribute {pkey} has correct value",
-                )
+                if not hasattr(self.rsa, pkey) or getattr(self.rsa, pkey) == None:
+                    no_param.add(pkey)
+                    continue
+                elif getattr(self.rsa, pkey) != getattr(param, pkey):
+                    wrong_param.add(pkey)
+                    continue
 
-    @weight(20)
+        error_param = no_param.union(wrong_param)
+        set_score(5 * (6 - len(error_param)))
+
+        self.assertTrue(
+            len(no_param) == 0,
+            f"Class does not have the attribute(s) <{', '.join(no_param)}>",
+        )
+        self.assertTrue(
+            len(wrong_param) == 0,
+            f"Class compute wrong attribute(s) <{', '.join(wrong_param)}>",
+        )
+
+    @partial_credit(20)
     @tags("output")
     @visibility("after_due_date")
     @number("2.2")
-    def test_encryption(self):
+    def test_encryption(self, set_score=None):
         """
         Check class method encrypt
         """
         self.import_checker()
+        self.rsa = RSA.RSA()
+        curr_score = 0
 
         for param in self.test_params.encryption:
-            self.rsa.compute_key(param.p, param.q, param.cp_index)
+            for pkey in ["p", "q", "n", "phi", "e", "d"]:
+                setattr(self.rsa, pkey, getattr(param, pkey))
             cipher_test = self.rsa.encrypt(param.msg)
             self.assertEqual(
-                cipher_test, param.cipher, f"Checking class method encrypt"
+                cipher_test, param.cipher, f"Class method encrypt output wrong result"
             )
+            curr_score += 4
+            set_score(curr_score)
 
-    @weight(20)
+    @partial_credit(20)
     @tags("output")
     @visibility("after_due_date")
     @number("2.3")
-    def test_decryption(self):
+    def test_decryption(self, set_score=None):
         """
         Check class method decrypt
         """
         self.import_checker()
+        self.rsa = RSA.RSA()
+        curr_score = 0
 
         for param in self.test_params.decryption:
-            self.rsa.compute_key(param.p, param.q, param.cp_index)
+            for pkey in ["p", "q", "n", "phi", "e", "d"]:
+                setattr(self.rsa, pkey, getattr(param, pkey))
             cipher_test = self.rsa.encrypt(param.msg)
             self.assertEqual(
-                cipher_test, param.cipher, f"Checking class method decrypt"
+                cipher_test, param.cipher, f"Class method encrypt output wrong result"
             )
             decrypted_msg = self.rsa.decrypt(param.cipher)
-            self.assertEqual(decrypted_msg, param.msg, f"Checking class method decrypt")
+            self.assertEqual(
+                decrypted_msg, param.msg, f"Class method decrypt output wrong result"
+            )
+            curr_score += 4
+            set_score(curr_score)
 
-    @weight(30)
+    @partial_credit(30)
     @tags("output")
     @visibility("after_due_date")
     @number("2.4")
-    def test_all(self):
+    def test_all(self, set_score=None):
         """
         Check all methods
         """
         self.import_checker()
+        self.rsa = RSA.RSA()
+        curr_score = 0
 
         for param in (
             self.test_params.compute_key
@@ -138,7 +166,11 @@ class TestOutput(unittest.TestCase):
             self.rsa.compute_key(param.p, param.q, param.cp_index)
             cipher_test = self.rsa.encrypt(param.msg)
             self.assertEqual(
-                cipher_test, param.cipher, f"Checking class method decrypt"
+                cipher_test, param.cipher, f"Class method encrypt output wrong result"
             )
             decrypted_msg = self.rsa.decrypt(param.cipher)
-            self.assertEqual(decrypted_msg, param.msg, f"Checking class method decrypt")
+            self.assertEqual(
+                decrypted_msg, param.msg, f"Class method decrypt output wrong result"
+            )
+            curr_score += 2
+            set_score(curr_score)
